404d3c456e67ef82cbb59ce91d9350f5866ae58d
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-07-18 13:51:52 +0200

    Etape 1 : demander à l’utilisateur chaque nombre constituant la liste ; par convention, la saisie de 0 indique que la saisie de la liste est terminée ;

--- exercice_1.py
+++ exercice_1.py
@@ -4,3 +4,12 @@
 """
 Exercice 1 Base Python
 """
+
+numbers = []
+while True:
+    number = int(input("Enter number "))
+    if number == 0:
+        break
+    numbers.append(number)
+
+print("List numbers:", numbers)
