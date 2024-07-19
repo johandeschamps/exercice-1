167c86c0c984cf81642b8c434c561949b08c4115
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-07-19 10:09:38 +0200

    Ajouts commentaires

--- exercice_1.py
+++ exercice_1.py
@@ -5,17 +5,22 @@
 Exercice 1 Base Python
 """
 
+# L'utilisateur rentre un nombre, Si le nombre saisi est égale à 0
+# l'utilisateur sort de la boucle
+# Sinon il ajoute le nombre à la liste "numbers".
+
 numbers = []
 while True:
     try:
-        number = int(input("Enter number "))
+        number = int(input("Enter number, 0 to exit : "))
         if number == 0:
             break
         numbers.append(number)
     except ValueError:
         print("Sorry, the input is not correct")
 
-# ----------------------------------------------
+# Après avoir collecté les nombres, le code vérifie s’il y a des nombres négatifs
+# dans la liste “numbers”, il itère à travers chaque nombre dans la liste.
 
 for num in numbers:
     if num < 0:
@@ -24,7 +29,8 @@ for num in numbers:
 else:
     print("No intruder number detected in the list.")
 
-# ----------------------------------------------
+# La liste appelée negative_numbers contient tous les nombres négatifs de
+# la liste numbers d’origine (s’il y en a).
 
 negative_numbers = [num for num in numbers if num < 0]
 
@@ -32,7 +38,10 @@ if negative_numbers:
     print(f"Intruded number(s) detected : {negative_numbers}")
     print(f"Total number of intruded number(s) : {len(negative_numbers)}")
 
-# ----------------------------------------------
+# La liste appelée positive_numbers contient tous
+# les nombres positifs de la liste numbers d’origine et l’affiche.
 
-positive_numbers = [num for num in numbers if num >= 0]
+positive_numbers = [num for num in numbers if num > 0]
 print(positive_numbers)
+
+# ----------------------------------------------------------------------------
